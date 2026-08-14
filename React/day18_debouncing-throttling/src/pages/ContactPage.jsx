3b463a3394075f97f1dc7c523b9c5050cc280f3f
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-08-14 08:50:14 +0530

    debouncing, throttling, and code-splitting concepts

---
+++ React/day18_debouncing-throttling/src/pages/ContactPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div className='flex h-[90vh] justify-center items-center text-4xl'>
+      This is contact page
+    </div>
+  );
+}
+
+export default ContactPage;
