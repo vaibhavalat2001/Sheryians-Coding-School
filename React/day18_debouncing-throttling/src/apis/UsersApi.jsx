e66a3c58f41777ee2cbec9306f67c08cd8ab9741
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-08-21 09:14:20 +0530

    del

--- React/day18_debouncing-throttling/src/apis/UsersApi.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getData = async () => {
-    console.log("Api loaded...");
-    let res = await axios.get("https://fakestoreapi.com/users");
-    console.log(res.data);
-  };
