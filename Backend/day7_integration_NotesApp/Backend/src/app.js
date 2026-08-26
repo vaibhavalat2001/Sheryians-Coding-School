a36798b9e33f4942571aae9b026db1cdc6358d1b
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-08-27 00:14:09 +0530

    added cors origin of live link

--- Backend/day7_integration_NotesApp/Backend/src/app.js
+++ Backend/day7_integration_NotesApp/Backend/src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://nottivo.vercel.app"
 }))
 connectDB();       
 
