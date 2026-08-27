bbf99578d8d36857ec40351fbe7cc78b7edeb756
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-08-27 10:26:27 +0530

    .env bd connection string edited

--- Backend/day7_integration_NotesApp/Backend/src/app.js
+++ Backend/day7_integration_NotesApp/Backend/src/app.js
@@ -1,25 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const notesRouter = require("./routes/notes.routes");
 const cors = require("cors");
 
 const app = express();
-
 app.use(express.json());
-
-app.use(
-    cors({
-        origin: "https://nottivo.vercel.app",
-    })
-);
-
-app.get("/", (req, res) => {
-    res.send("Nottivo Backend is Live 🚀");
-});
+app.use(cors({
+    origin: "https://nottivo.vercel.app"
+}))
+connectDB();       
 
 app.get("/notes", (req, res) => {
     res.send("Backend connected");
-});
+})
 
-app.use("/notes", notesRouter);
+app.use("/notes", notesRouter)
 
 module.exports = app;
