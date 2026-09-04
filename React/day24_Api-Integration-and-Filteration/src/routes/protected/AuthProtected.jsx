e7d2f5737f3bf9a72086bc94668f317bd0b9636c
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-09-04 13:58:34 +0530

    added loader

--- React/day24_Api-Integration-and-Filteration/src/routes/protected/AuthProtected.jsx
+++ React/day24_Api-Integration-and-Filteration/src/routes/protected/AuthProtected.jsx
@@ -5,7 +5,20 @@ import { Navigate, Outlet } from "react-router";
 const AuthProtected = () => {
   const { user, isLoading } = useSelector((store) => store.auth);
 
-  if (isLoading) return <h1>Loading State...</h1>
+  if (isLoading)
+    return (
+      <div className="bg-black h-screen">
+        <div className="flex min-h-[80vh] items-center justify-center">
+          <div className="relative h-14 w-14">
+            <div className="absolute inset-0 rounded-full border-4 border-[#262626]" />
+
+            <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-purple-500" />
+
+            <div className="absolute inset-3 rounded-full bg-[#0d0d0d]" />
+          </div>
+        </div>
+      </div>
+    );
 
   if (user) {
     return <Navigate to={"/main"} />;
--- React/day24_Api-Integration-and-Filteration/src/routes/protected/MainProtected.jsx
+++ React/day24_Api-Integration-and-Filteration/src/routes/protected/MainProtected.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router";
 
 const MainProtected = () => {
-   const {user, isLoading} = useSelector((store) => store.auth)
+  const { user, isLoading } = useSelector((store) => store.auth);
 
-   if (isLoading) return <h1>Loading state...</h1>
- 
-   if (!user) {
-    return <Navigate to={"/"}/>
-   }
+  if (isLoading)
+    return (
+      <div className="bg-black h-screen">
+        <div className="flex min-h-[80vh] items-center justify-center">
+          <div className="relative h-14 w-14">
+            <div className="absolute inset-0 rounded-full border-4 border-[#262626]" />
+
+            <div className="absolute inset-0 animate-spin rounded-full border-4 border-transparent border-t-purple-500" />
+
+            <div className="absolute inset-3 rounded-full bg-[#0d0d0d]" />
+          </div>
+        </div>
+      </div>
+    );
+
+  if (!user) {
+    return <Navigate to={"/"} />;
+  }
   return (
     <div>
-      <Outlet/>
+      <Outlet />
     </div>
   );
-}
+};
 
 export default MainProtected;
