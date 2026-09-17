d8dbe3703862fb0683aff8200d3814fa75c11465
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-09-17 17:58:59 +0530

    added live url of server

--- Backend/day18_Url-Shortner-project/Frontend/vite.config.js
+++ Backend/day18_Url-Shortner-project/Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://urlshort-backend.vercel.app/",
         changeOrigin: true,
       },
     },
