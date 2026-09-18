7da310c358f7fafd4704bc3df0994d65510966e3
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-09-18 15:07:07 +0530

    express validator, validate email, phone, and password

---
+++ Backend/day19_Express_validation/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import config from "./config.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGO_URI);    
+
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.log("error while connect mongodb");
+  }
+};
+
+export default connectDB;
