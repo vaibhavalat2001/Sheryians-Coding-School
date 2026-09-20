3a05eb36c15b011ad7cc9a5ac66c9aa460cbf009
vaibhav gajanan alat <vaibhavalat@gmail.com>
2026-09-20 18:27:44 +0530

    fix: update CORS origin to allow requests from production frontend

--- Backend/day19_Url-Shortner-project/Backend/src/app/app.js
+++ Backend/day19_Url-Shortner-project/Backend/src/app/app.js
@@ -5,12 +5,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(express.json());  
+app.use(express.json());
 
 app.use(
   cors({
-    // origin: "https://shortnerurl-frontend.vercel.app",
-    origin: "http://localhost:5173/",
+    origin: "https://shortnerurl-frontend.vercel.app",
     credentials: true,
   }),
 );
